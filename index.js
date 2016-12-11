2f6e5a934e1731309e2a9b1bf4d7efb762657263
orhanbalci <orhanbalci@gmail.com>
2016-12-11 21:21:14 +0300

    gitignore added

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 var net = require('net');
 var protobuf = require('protocol-buffers')
 const fs = require('fs')
+var readline = require('readline')
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 var messages = protobuf(fs.readFileSync('protocol.proto'))
 
 var HOST = '127.0.0.1';
 var PORT = 4242;
 
-var stdin = process.openStdin();
-//var stdOut = process.openStdout();
+
 
 var encodedBuffer = messages.BasRequestResponse.encode({
   request_type: 0,
@@ -28,9 +33,25 @@ client.connect(PORT, HOST, function() {
 // data is what the server sent to this socket
 client.on('data', function(data) {
     
-    console.log('Server Response: ' + data);
+	var serverMessage =  messages.BasRequestResponse.decode(data)
+    console.log('Server Response: ' + serverMessage.request_type);
     // Close the client socket completely
     //client.destroy();
+	if (serverMessage.request_type == 2)
+	{
+		rl.question('Please tell me your name: ', function (name) {	
+     
+			client.write(messages.BasRequestResponse.encode({
+				request_type : 3,
+				name : name
+			}));
+			rl.close();        
+		});
+     
+	}else if(serverMessage.request_type == 6){
+		console.log("New Player  added to table " + serverMessage.name + "to direction " + serverMessage.user_direction)
+	}
+	
     
 });
 
@@ -39,11 +60,9 @@ client.on('close', function() {
     console.log('Connection closed');
 });
 
-stdin.addListener("data", function(d){
-	client.write(d);
-});
-
 process.on('SIGINT', function() {
+	//process.stdin.destroy();
+	console.log('Destroying app')
   client.end();
   client.destroy();
   process.exit();
